506006c3cf7afb905cf3e16311d05be5f62d28f3
ShubhamOfficial5505 <shubhamkarofficial5505@gmail.com>
2025-11-21 18:18:14 +0530

    Initial Commit

---
+++ Basics/FibonacciN.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class FibonacciN {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = scn.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int sum;
+
+        int counter = 1; // Iterator
+        while(counter <= (n + 1)) {
+            System.out.println(a);
+            
+            sum = a + b;
+            a = b;
+            b = sum;
+
+            counter++;
+        }
+    }
+}
