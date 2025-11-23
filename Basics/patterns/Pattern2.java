1b1d63b81e638e76925710b521e63da54b828802
ShubhamOfficial5505 <shubhamkarofficial5505@gmail.com>
2025-11-23 12:52:50 +0530

    Add Pattern2.java

---
+++ Basics/patterns/Pattern2.java
@@ -0,0 +1,29 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = scn.nextInt();
+
+        int row = 1;
+        while(row <= n) {
+            // work
+            int cst = 1; // count of stars
+            int nst = row; // number of stars
+            while(cst <= nst) {
+                System.out.print("*");
+                cst++;
+            }
+
+            // preparation
+            System.out.print("\n");
+            row++;
+        }
+
+        scn.close();
+    }
+}
