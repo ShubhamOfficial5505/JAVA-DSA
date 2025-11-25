f2da18d946977a888b0ac011daed0012d94402ac
ShubhamOfficial5505 <shubhamkarofficial5505@gmail.com>
2025-11-25 10:04:55 +0530

    Add Pattern6.java

--- Basics/patterns/Pattern6.java
+++ Basics/patterns/Pattern6.java
@@ -2,27 +2,20 @@ package patterns;
 
 import java.util.Scanner;
 
-public class Pattern5 {
+public class Pattern6 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
 		System.out.print("Enter n: ");
 		int n = scn.nextInt();
 		
-		int nsp = 0;
-		int nst = n;
+		int nst = 1;
 		
 		// rows
+		int nr = (2 * n) - 1;
 		int row = 1;
-		while(row <= n) {
-			// spaces work
-			int csp = 1;
-			while(csp <= nsp) {
-				System.out.print(" ");
-				csp++;
-			}
-			
-			// stars work
+		while(row <= nr) {
+			// work
 			int cst = 1;
 			while(cst <= nst) {
 				System.out.print("*");
@@ -31,8 +24,11 @@ public class Pattern5 {
 			
 			// preparation
 			System.out.print("\n");
-			nsp++;
-			nst--;
+			if(row <= (nr/2)) {
+				nst++;
+			} else {
+				nst--;
+			}
 			row++;
 		}
 		
