4608b68c169b39c50b0d5441309a28fc5442be16
ShubhamOfficial5505 <shubhamkarofficial5505@gmail.com>
2025-11-27 12:24:37 +0530

    Add Pattern27.java

---
+++ Basics/patterns/Pattern27.java
@@ -0,0 +1,46 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern27 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("Enter n: ");
+		int n = scn.nextInt();
+		
+		int nst = n;
+		int nsp = -1;
+		
+		// rows
+		int row = 1;
+		while(row <= n) {
+			// stars work 1
+			for(int cst = 1; cst <= nst; cst++) {
+				System.out.print("*");
+			}
+			
+			// spaces work
+			for(int csp = 1; csp <= nsp; csp++) {
+				System.out.print(" ");
+			}
+			
+			// stars work 2
+			int cst = 1;
+			if(row == 1) {
+				cst = 2;
+			}
+			for(; cst <= nst; cst++) {
+				System.out.print("*");
+			}
+			
+			// preparation
+			System.out.print("\n");
+			nst -= 1;
+			nsp += 2;
+			row += 1;
+		}
+		
+		scn.close();
+	}
+}
